c6bb3aefc2175fafd3580b74e5fcd59dff8403a8
Ben Tomsett <ben@tomsett.xyz>
2022-03-31 10:48:25 +0100

    Add ESLint and Prettier for code style/linting

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
