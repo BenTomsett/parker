1a0d6ab4ffba3cf7aa9c3b77b1a614930f99d5de
Ben Tomsett <ben@tomsett.xyz>
2022-03-31 11:56:49 +0100

    Boilerplate code for login/registration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
