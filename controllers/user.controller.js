3b01761492db33d883433e6371aa92e7f30ef52a
James-Kerrison-UEA <j.kerrison@uea.ac.uk>
2022-04-09 23:59:10 +0100

    Create user.controller.js
    Added user.controller.js

---
+++ controllers/user.controller.js
@@ -0,0 +1,189 @@
+/*  CMP5012B - Software Engineering - Coursework 2 -  Parker
+    Author: Bradley Crisp - Group 12
+    IDE Version: Jetbrains Webstorm
+    Current Version: Managed by GitHub
+    Date Created: 09/04/2022
+    Date Finished:
+    Last Modified: 00:00 10/04/2022
+
+ -------DESCRIPTION-------
+
+This is the accounts controller which handles the processing of data for the accounts.
+It does this mainly through database interactions and has all CRUD functions which are
+accessed via the accounts routes.
+
+*/
+const { Op } = require("sequelize");
+
+const User = require('../models/user.model');
+const AdminUser = require('../models/adminuser.model');
+
+// Create and Save a new user account to the database
+const createUser = async (req, res) => {
+  const user = req.body;
+
+  User.create(user, {
+    fields: [
+      'userId',
+      'forename',
+      'surname',
+      'dob',
+      'email',
+      'password',
+      'userId',
+      'addressLine1',
+      'addressLine2',
+      'city',
+      'postcode',
+      'country',
+    ],
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        res.status(409).send('ERR_USER_EXISTS');
+      } else if (err.name === 'SequelizeValidationError') {
+        res.status(400).send('ERR_DATA_MISSING');
+      } else {
+        console.err(err);
+        res.status(500).send('ERR_INTERNAL_EXCEPTION');
+      }
+    });
+};
+
+// Create and save a new admin user to the database
+const createAdminUser = async (req, res) => {
+  const adminUser = req.body;
+
+  AdminUser.create(adminUser, {
+    fields: [
+      'userId',
+      'forename',
+      'surname',
+      'dob',
+      'email',
+      'password',
+      'userId',
+      'addressLine1',
+      'addressLine2',
+      'city',
+      'postcode',
+      'country',
+    ],
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        res.status(409).send('ERR_USER_EXISTS');
+      } else if (err.name === 'SequelizeValidationError') {
+        res.status(400).send('ERR_DATA_MISSING');
+      } else {
+        console.err(err);
+        res.status(500).send('ERR_INTERNAL_EXCEPTION');
+      }
+    });
+};
+
+
+
+// Retrieve all users from the database
+const findAllUsers = async (req, res) => {
+  User.findAll()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+      res.status(500).send('ERR_INTERNAL_EXCEPTION');
+    });
+};
+
+// Retrieve all admin users from the database
+const findAllAdminUsers = async (req, res) => {
+  AdminUser.findAll()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+      res.status(500).send('ERR_INTERNAL_EXCEPTION');
+    });
+};
+
+// Finds a user in the DB using their userID.
+const findUser = async (req, res) => {
+  const userID = req.params.userId;
+
+  userID.findByPk(userID)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+      res.status(500).send('ERR_INTERNAL_EXCEPTION');
+    });
+};
+
+// Update a user by the id in the request
+const updateUser = async (req, res) => {
+  const { userId } = req.params;
+
+  User.update(req.body, { where: { userId } })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        res.status(409).send('ERR_USER_EXISTS');
+      } else if (err.name === 'SequelizeValidationError') {
+        res.status(400).send('ERR_DATA_MISSING');
+      } else {
+        console.err(err);
+        res.status(500).send('ERR_INTERNAL_EXCEPTION');
+      }
+    });
+};
+
+// Delete a user with the specified id in the request
+const deleteUser = async (req, res) => {
+  const { userId } = req.params;
+
+  User.destroy({ where: { userId } })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+      res.status(500).send('ERR_INTERNAL_EXCEPTION');
+    });
+};
+
+// Delete all users from the database.
+const deleteAllUsers = async (req, res) => {
+  User.destroy({
+    where: {},
+    truncate: false,
+  })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.err(err);
+      res.status(500).send('ERR_INTERNAL_EXCEPTION');
+    });
+};
+
+module.exports = {
+  createUser,
+  createAdminUser,
+  findAllUsers,
+  findAllAdminUsers,
+  findUser,
+  updateUser,
+  deleteUser,
+  deleteAllUsers,
+};
