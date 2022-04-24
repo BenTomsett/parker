4f90fc4ce65eb6b0baa65f8abfaa7d4599029aaa
Ben Tomsett <ben@tomsett.xyz>
2022-04-24 21:49:43 +0100

    finalise /auth/login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,37 +1,8 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-
-const User = require('../models/user.model');
-const { generateToken } = require('../utils/auth');
+const AuthController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  const users = await User.findAll({
-    where: { email },
-    attributes: ['forename', 'surname', 'email', 'password'],
-  });
-
-  if (users.length < 1) {
-    res.status(401).send('ERR_INVALID_CREDENTIALS');
-  } else if (users.length > 1) {
-    res.status(500).send('ERR_MULTIPLE_USERS');
-  } else {
-    const user = users[0];
-
-    bcrypt.compare(password, user.password, (err, result) => {
-      if (err) {
-        res.status(500).send('ERR_INTERNAL_EXCEPTION');
-      } else if (result) {
-        const token = generateToken(user);
-        res.status(200).send(token);
-      } else {
-        res.status(401).send('ERR_INVALID_CREDENTIALS');
-      }
-    });
-  }
-});
+router.post('/login', AuthController.getTokenFromCredentials);
 
 module.exports = router;
