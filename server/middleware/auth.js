24e238836e9d91e59db34f9522d44ab92cbaca96
Ben Tomsett <ben@tomsett.xyz>
2022-05-07 11:45:30 +0100

    allow use of cookies for sending auth tokens

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,11 +2,16 @@ const { verifyToken } = require('../utils/auth');
 const User = require('../models/user.model');
 
 const authenticateUser = async (req, res, next) => {
-  if (!req.headers.authorization) {
+  if (!req.headers.authorization && !req.cookies.token) {
     return res.status(401).send('ERR_UNAUTHORIZED');
   }
-  const token = req.headers.authorization.split(' ')[1];
-  if (!token) {
+
+  let token;
+  if(req.headers.authorization && req.headers.authorization.split(' ')[1]){
+    token = req.headers.authorization.split(' ')[1];
+  }else if(req.cookies.token){
+    token = req.cookies.token;
+  }else{
     return res.status(401).send('ERR_UNAUTHORIZED');
   }
 
