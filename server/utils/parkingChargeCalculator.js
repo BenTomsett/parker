54fc8124bdcb0b6bb3f6f9610571e1570f3f4882
hubbardliam <77333314+hubbardliam@users.noreply.github.com>
2022-05-09 21:45:55 +0100

    Initial Parking charge calculator
    Simple function that returns the charge based off booking start & end dates.

---
+++ server/utils/parkingChargeCalculator.js
@@ -0,0 +1,19 @@
+/*  CMP5012B - Software Engineering - Coursework 2 -  Parker
+    Author: Liam Hubbard - Group 12
+    IDE Version: Jetbrains Webstorm
+    Current Version: Managed by GitHub
+    Date Created: 09/05/2022
+    Date Finished:
+    Last Modified: 09/05/2022 - Liam H
+
+ -------DESCRIPTION-------
+
+This is the parking charge util which handles all functions relating to calculating booking costs
+
+ */
+
+function calculateParkingCharge(Booking){
+    const flatRate = 3.50
+    let duration = Math.abs(Booking.endDate - Booking.startDate) / 36e5 //Calculates duration in hours
+    return (duration * flatRate).toFixed(2)
+}
