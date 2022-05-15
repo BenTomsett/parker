5ac10b588c495807eb4ad1bf1df95edf5b6a3146
Ben Tomsett <ben@tomsett.xyz>
2022-05-15 08:31:14 +0100

    update delete user modal

--- app/src/pages/admin/users/BanUser.jsx
+++ app/src/pages/admin/users/BanUser.jsx
@@ -3,61 +3,65 @@
 
 import React from 'react';
 import {
-    Button,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay,
-    useDisclosure,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from '@chakra-ui/react';
 
-const BanUser = ({user, update}) => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
+const BanUser = ({ user, update }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const toggleBanUser = async () => {
-        if (user.isBanned) {
-            return fetch(`/api/users/${user.userId}/unban`, {
-                method: 'PUT',
-            })
-        } else {
-            return fetch(`/api/users/${user.userId}/ban`, {
-                method: 'PUT',
-            })
-        }
+  const toggleBanUser = async () => {
+    if (user.isBanned) {
+      return fetch(`/api/users/${user.userId}/unban`, {
+        method: 'PUT',
+      });
+    } else {
+      return fetch(`/api/users/${user.userId}/ban`, {
+        method: 'PUT',
+      });
     }
+  };
 
-    return (
-        <>
-            <Button colorScheme='orange' onClick={onOpen}>{user.isBanned ? "Unban" : "Ban"} user</Button>
+  return (
+    <>
+      <Button colorScheme='orange' onClick={onOpen}>{user.isBanned ?
+        'Unban' :
+        'Ban'} user</Button>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay/>
-                <ModalContent>
-                    <ModalHeader>{user.isBanned ? "Unban" : "Ban"} user</ModalHeader>
-                    <ModalCloseButton/>
-                    <ModalBody>
-                        <h2> Are you sure you want to {user.isBanned ? "unban" : "ban"} {user.forename} {user.surname}?</h2>
-                    </ModalBody>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{user.isBanned ? 'Unban' : 'Ban'} user</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <h2> Are you sure you want to {user.isBanned ?
+              'unban' :
+              'ban'} {user.forename} {user.surname}?</h2>
+          </ModalBody>
 
-                    <ModalFooter>
-                        <Button colorScheme='red' mr={3} onClick={async () => {
-                            await toggleBanUser(user);
-                            update();
-                            onClose();
-                        }}>
-                            {user.isBanned ? "Unban" : "Ban"} user
-                        </Button>
-                        <Button mr={3} onClick={onClose}>
-                            No
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    )
-}
+          <ModalFooter>
+            <Button colorScheme='red' mr={3} onClick={async () => {
+              await toggleBanUser(user);
+              update();
+              onClose();
+            }}>
+              {user.isBanned ? 'Unban' : 'Ban'} user
+            </Button>
+            <Button mr={3} onClick={onClose}>
+              No
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 export default BanUser;
