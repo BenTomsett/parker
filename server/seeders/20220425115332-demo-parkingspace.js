00f9602587e5ba19674fc3dce122d0a1aeaf4d74
Bradley Crisp <53059802+b-crisp@users.noreply.github.com>
2022-05-15 14:40:34 +0100

    Modified parkingspaces to use point geometry. Changed seeders to suit.

--- server/seeders/20220425115332-demo-parkingspace.js
+++ server/seeders/20220425115332-demo-parkingspace.js
@@ -1,18 +1,18 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    const ps1polygon = {type: 'Polygon', coordinates: [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0],[100.0, 1.0],[100.0, 0.0]]]};
-    const ps2polygon = {type: 'Polygon', coordinates: [[[101.0, 0.0], [102.0, 0.0], [102.0, 1.0],[101.0, 1.0],[101.0, 0.0]]]}
-    const ps3polygon = {type: 'Polygon', coordinates: [[[102.0, 0.0], [103.0, 0.0], [103.0, 1.0],[102.0, 1.0],[102.0, 0.0]]]}
-    const ps4polygon = {type: 'Polygon', coordinates: [[[103.0, 0.0], [104.0, 0.0], [104.0, 1.0],[103.0, 1.0],[103.0, 0.0]]]}
-    const ps5polygon = {type: 'Polygon', coordinates: [[[104.0, 0.0], [105.0, 0.0], [105.0, 1.0],[104.0, 1.0],[104.0, 0.0]]]}
-    const ps6polygon = {type: 'Polygon', coordinates: [[[105.0, 0.0], [106.0, 0.0], [106.0, 1.0],[105.0, 1.0],[105.0, 0.0]]]}
-    const ps7polygon = {type: 'Polygon', coordinates: [[[106.0, 0.0], [107.0, 0.0], [107.0, 1.0],[105.0, 1.0],[105.0, 0.0]]]}
-    const ps8polygon = {type: 'Polygon', coordinates: [[[107.0, 0.0], [108.0, 0.0], [108.0, 1.0],[107.0, 1.0],[107.0, 0.0]]]}
-    const ps9polygon = {type: 'Polygon', coordinates: [[[108.0, 0.0], [109.0, 0.0], [109.0, 1.0],[108.0, 1.0],[108.0, 0.0]]]}
-    const ps10polygon = {type: 'Polygon', coordinates: [[[109.0, 0.0], [110.0, 0.0], [110.0, 1.0],[109.0, 1.0],[109.0, 0.0]]]}
-    const ps11polygon = {type: 'Polygon', coordinates: [[[110.0, 0.0], [111.0, 0.0], [111.0, 1.0],[110.0, 1.0],[110.0, 0.0]]]}
-    const ps12polygon = {type: 'Polygon', coordinates: [[[111.0, 0.0], [112.0, 0.0], [112.0, 1.0],[111.0, 1.0],[111.0, 0.0]]]}
+    const ps1polygon = {type: 'Point', coordinates: [1.241371, 52.623380]};
+    const ps2polygon = {type: 'Point', coordinates: [1.241371, 52.623361]}; 
+    const ps3polygon = {type: 'Point', coordinates: [1.241373, 52.623341]}; 
+    const ps4polygon = {type: 'Point', coordinates: [1.241382, 52.623317]};
+    const ps5polygon = {type: 'Point', coordinates: [1.233338, 52.622387]};
+    const ps6polygon = {type: 'Point', coordinates: [1.233379, 52.622386]};
+    const ps7polygon = {type: 'Point', coordinates: [1.233379, 52.622386]};
+    const ps8polygon = {type: 'Point', coordinates: [1.233447, 52.622372]};
+    const ps9polygon = {type: 'Point', coordinates: [1.233467, 52.622384]};
+    const ps10polygon = {type: 'Point', coordinates: [1.233496, 52.622391]};
+    const ps11polygon = {type: 'Point', coordinates: [1.235344, 52.621352]};
+    const ps12polygon = {type: 'Point', coordinates: [1.235379, 52.621351]};
 
     await queryInterface.bulkInsert('ParkingSpaces', 
     [
